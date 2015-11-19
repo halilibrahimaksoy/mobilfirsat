bad6d85f2b6ec5b6ceaba724461a4a29073d815b
Tuba <tubatatli105@gmail.com>
2015-11-19 16:28:18 +0200

    fragment model ve getlist

--- app/src/main/java/com/tecpoint/mobilfirsat/mobilfirsat/Fragment/MainList.java
+++ app/src/main/java/com/tecpoint/mobilfirsat/mobilfirsat/Fragment/MainList.java
@@ -6,19 +6,34 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.webkit.WebView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import com.parse.FindCallback;
+import com.parse.GetCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+import com.tecpoint.mobilfirsat.mobilfirsat.Model.Category;
+import com.tecpoint.mobilfirsat.mobilfirsat.Model.Feed;
+import com.tecpoint.mobilfirsat.mobilfirsat.Model.FeedItem;
 import com.tecpoint.mobilfirsat.mobilfirsat.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class MainList extends Fragment {
 
-private ListView lsvMainList ;
-    public MainList() {
+    private ListView lsvMainList;
+    private String CategoryName;
+    private String CotegoryObjectId;
+    List<FeedItem> feedItemList;
+
+    public MainList(String CategoryName) {
+        this.CategoryName = CategoryName;
         // Required empty public constructor
     }
 
@@ -26,13 +41,58 @@ private ListView lsvMainList ;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        lsvMainList=(ListView)getActivity().findViewById(R.id.lsvMainList);
-        String[]dizi={"TUGBA","asjdkfl"};
-        ArrayAdapter<String> adapter= new ArrayAdapter<String>(getActivity().getApplicationContext(),android.R.layout.simple_list_item_1,android.R.id.text1,dizi);
-        lsvMainList.setAdapter(adapter);
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main_list, container, false);
+        View view = inflater.inflate(R.layout.fragment_main_list, container, false);
+        lsvMainList = (ListView) getActivity().findViewById(R.id.lsvMainList);
+        feedItemList = new ArrayList<FeedItem>();
+getList();
+        
+
+        return view;
     }
 
+    private void getList() {
+
+        final ParseQuery<Category> query = new ParseQuery<Category>(Category.class);
+        query.whereEqualTo(CategoryName, "Name");
+        query.getFirstInBackground(new GetCallback<Category>() {
+            @Override
+            public void done(Category object, ParseException e) {
+                CotegoryObjectId = object.getString("objectId");
+            }
+        });
+
+
+        ParseQuery<Feed> feedParseQuery = new ParseQuery<Feed>(Feed.class);
+        feedParseQuery.whereEqualTo(CotegoryObjectId, "CategoryId");
+        feedParseQuery.findInBackground(new FindCallback<Feed>() {
+            @Override
+            public void done(List<Feed> objects, ParseException e) {
+                for (Feed feed : objects) {
+                    final FeedItem feedItem = new FeedItem();
+                    feedItem.setTitle(feed.getString("Title"));
+                    feedItem.setDescription(feed.getString("Description"));
+                    feedItem.setSendDate(feed.getDate("createdAt"));
+                    feedItem.setLike("184");
+                    feedItem.setDislike("15");
+                    feedItem.setSikayet("9");
+
+                    ParseQuery<ParseUser> userParseQuery = ParseUser.getQuery();
+                    userParseQuery.whereEqualTo("objectId", feed.getString("UserId"));
+                    userParseQuery.findInBackground(new FindCallback<ParseUser>() {
+                        @Override
+                        public void done(List<ParseUser> objects, ParseException e) {
+                            for (ParseUser user : objects) {
+                                feedItem.setUserName(user.getString("username"));
+                                feedItem.setUserProfileImageUrl(user.getString("ProfilImage"));
+                            }
+                        }
+                    });
+
+
+                }
+            }
+        });
+
+    }
 
 }
